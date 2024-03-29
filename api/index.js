a1349d679d2ec1b90696aad21748f826b3186ebe
krongdew <dewwiisunny14@gmai.com>
2024-03-29 14:00:38 +0700

    first commit

---
+++ api/index.js
@@ -0,0 +1,82 @@
+import express from "express";
+import serverless from "serverless-http";
+import cors from "cors";
+import { fetchTasks, createTasks, updateTasks, deleteTasks } from "./task.js";
+
+
+
+const app = express()
+const port = 3001
+
+app.use(express.json())
+
+if(process.env.DEVELOPMENT) { 
+   app.use(cors()) 
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/task", async (req, res) => {
+    try {
+        
+      const tasks = await fetchTasks();
+      res.send(tasks.Items);
+      
+    } catch (err) {
+        
+      res.status(400).send(`Error fetching tasks: ${err}`);
+    }
+  });
+  
+app.post("/task", async (req, res) => {
+    try {
+        const task = req.body;
+        
+        const respone = await createTasks(task);
+        
+        res.send(respone)
+        
+      } catch (err) {
+        
+        res.status(400).send(`Error creating tasks: ${err}`);
+    }
+  })
+
+app.put('/task', async (req, res) => {
+    try {
+        const task = req.body;
+        
+        const respone = await updateTasks(task);
+        
+        res.send(respone)
+        
+      } catch (err) {
+        
+        res.status(400).send(`Error updating tasks: ${err}`);
+    }
+  })
+  
+app.delete('/task/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        
+        const respone = await deleteTasks(id);
+        
+        res.send(respone)
+        
+      } catch (err) {
+        
+        res.status(400).send(`Error deleting tasks: ${err}`);
+    }
+  })
+
+
+    
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+export const handler = serverless(app)
---
+++ ui/src/component/UpdateTaskForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Button, Dialog, DialogTitle, TextField } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import axios from "axios";
+import { API_URL } from "../../utils";
+
+export const UpdateTaskForm = ({ fetchTasks, isDialogOpen, setIsDialogOpen, task }) => {
+  const { id, completed } = task;
+  const [taskname, setTaskName] = useState("");
+    
+  const handdleUpdateTaskName = async () => {
+    try {
+        await axios.put(API_URL, {
+            id, 
+            name: taskname,
+            completed,
+        })
+        
+        await fetchTasks();
+        setTaskName("");
+        
+    } catch (err) {
+        console.log(err)
+    }
+  }
+  return (
+    <Dialog open={isDialogOpen}>
+      <DialogTitle>Edit Task</DialogTitle>
+      <div className="dialog">
+        <TextField
+          size="small"
+          label="Task"
+          variant="outlined"
+          onChange={(e) => setTaskName(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          onClick={ async () => {
+            await handdleUpdateTaskName()
+            setIsDialogOpen(false);
+          }}
+        >
+            <CheckIcon/>
+        </Button>
+      </div>
+    </Dialog>
+  );
+};
